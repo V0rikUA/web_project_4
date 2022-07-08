3dcfb2acf939756d4a1a13fb9af45101afe6325d
Dmitriy Kirov <kirovdmitriy@gmail.com>
2022-07-08 22:57:11 +0300

    feat: add section component to render new items

--- scripts/Section.js
+++ scripts/Section.js
@@ -1,3 +1,16 @@
-export default class Section{
-    constructor({item, renderer})
+export default class Section {
+  constructor({ item, renderer }, containerSelector) {
+    this._item = item;
+    this._renderer = renderer;
+    this._container = document.querySelector(containerSelector);
+  }
+
+  renderer() {
+    const newElement = this._renderer(this._item);
+    this.addItem(newElement);
+  }
+
+  addItem(element) {
+    this._container.prepend(element);
+  }
 }
