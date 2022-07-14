7f091adfd3f84b295c2b41ebc12ae67fb249ff7b
Dmitriy Kirov <kirovdmitriy@gmail.com>
2022-07-14 18:24:34 +0300

    commnets were added

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,13 +1,23 @@
-export default class UserInput {
-  constructor({ userNameSelector, userJobSelector }) {
+export default class UserInfo {
+  constructor(userNameSelector, userJobSelector) {
     this._userNameElement = document.querySelector(userNameSelector);
     this._userProfessionElement = document.querySelector(userJobSelector);
   }
 
+  /**
+   * get user info from DOM
+   * @returns as an object
+   */
   getUserInfo() {
     return { userName: this._userNameElement.textContent, userJob: this._userProfessionElement.textContent };
   }
 
+  /**
+   * Set strings to the DOM
+   * 
+   * @param {String} userName 
+   * @param {String} userJob 
+   */
   setUserInfo(userName, userJob) {
     this._userNameElement.textContent = userName;
     this._userProfessionElement.textContent = userJob;
