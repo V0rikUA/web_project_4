a4cccee8277eab72750a4dd03a4b2298a8b188f6
Dmitriy Kirov <kirovdmitriy@gmail.com>
2022-07-14 18:23:22 +0300

    optimized naming of returned object elemnts

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -9,22 +9,26 @@ export default class PopupWithForm extends Popup {
   constructor(popupSelector, handleFormSubmit) {
     super(popupSelector);
     this._handleFormSubmit = handleFormSubmit;
-    this._form = super._popupElement.querySelector("form");
+    this._form = this._popupElement.querySelector("form");
   }
   /**
    *collects data from all the input fields and
-   * @returns an object with data
+   * @returns an object with data.
+   *
    */
   _getInputValues() {
     return Array.from(this._popupElement.querySelectorAll("input")).reduce(
-      (acc, input) => ({ ...acc, [input.id]: input.value }),
+      (acc, input) => ({ ...acc, [input.id.split("-")[0]]: input.value }),
       {}
     );
   }
 
   setEventListeners() {
     super.setEventListeners();
-    this._form.addEventListener("submit", handleFormSubmit);
+    this._form.addEventListener("submit", (event) => {
+      const inputValues = this._getInputValues();
+      this._handleFormSubmit(event, inputValues);
+    });
   }
 
   /**
