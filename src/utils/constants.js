e390737b5b0413b4f8b52d9a2a7209fe5f9693d2
Dmitriy Kirov <kirovdmitriy@gmail.com>
2022-07-30 13:27:31 +0300

    adeed new constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,16 +1,26 @@
 //credentialss variables
-export const profileEditButton = document.querySelector(".profile__button-edit"); //
+export const profileEditButton = document.querySelector(
+  ".profile__button-edit"
+); //
 export const profileNameSelector = ".profile__name"; //
 export const profileProfessionSelector = ".profile__profession"; //
+export const profileAvatarSelector = ".profile__avatar";
+export const popupAvatarChangeSelector = ".popup_type_change-avatar";
 export const popupProfileSelector = ".popup_type_profile"; //
 export const userInputName = document.querySelector(".form__input_type_name"); //
-export const userInputProfession = document.querySelector(".form__input_type_title"); //
+export const userInputProfession = document.querySelector(
+  ".form__input_type_title"
+); //
 
 //NewCard variables
-export const addGalleryItemButton = document.querySelector(".profile__button-add-contetnt"); //
+export const addGalleryItemButton = document.querySelector(
+  ".profile__button-add-contetnt"
+); //
 export const galleryItemsListSelector = ".gallery-list"; //
 export const newItemPopupSelector = ".popup_type_new-gallery-item"; //
-export const formAddGalleryItem = document.querySelector(".form_type_new-gallery-item");
+export const formAddGalleryItem = document.querySelector(
+  ".form_type_new-gallery-item"
+);
 
 export const initialCards = [
   {
@@ -46,4 +56,7 @@ export const initialCards = [
 ];
 
 //Prewiev variable
-export const previewPopupSelector = ".popup_type_preview"; //
+export const previewPopupSelector = ".popup_type_preview";
+
+//loading animation
+export const loadingPopup = document.querySelector(".popup__loading");
